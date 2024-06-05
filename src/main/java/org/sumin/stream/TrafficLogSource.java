3bcadeeb432efbd6dfbd34ce5dafdefb3a733a51
Eeap <kdg97811@naver.com>
2024-06-05 22:49:05 +0900

    refactor: divide trafficlog class for processing data

--- src/main/java/org/sumin/stream/TrafficLogSource.java
+++ src/main/java/org/sumin/stream/TrafficLogSource.java
@@ -4,34 +4,39 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.apache.flink.api.common.typeinfo.TypeInfo;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.List;
 import java.util.TimeZone;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class TrafficLog {
+public class TrafficLogSource {
     public String vpcId;
     public String subnetId;
     public String interfaceId;
-    @TypeInfo(PacketTypeInfo.class)
-    public List<Integer> packets;
-    public String start;
+    public String logStatus;
+    public int packets;
+    public int bytes;
+    public long start;
+    public long end;
+    public String eventTime;
     public Long getTime() throws ParseException {
         SimpleDateFormat setTime = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS Z");
         setTime.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
-        return setTime.parse(this.start).toInstant().toEpochMilli();
+        return setTime.parse(this.eventTime).toInstant().toEpochMilli();
     }
     public String toString() {
         return "Log vpcId: " + this.vpcId +
                 " Log subnetId: " + this.subnetId +
                 " Log interfaceId: " + this.interfaceId +
+                " Log logStatus: " + this.logStatus +
                 " Log packets: " + this.packets +
-                " Log start: " + this.start;
+                " Log bytes: " + this.bytes +
+                " Log start: " + this.start +
+                " Log end: " + this.end +
+                " Log eventTime: " + this.eventTime;
     }
 }
