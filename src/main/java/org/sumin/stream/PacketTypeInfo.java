7bc284e6b8dbe8ddc51c0f577f2080ce4cf8998f
Eeap <kdg97811@naver.com>
2024-06-06 14:42:25 +0900

    fix: fix integer type info

--- src/main/java/org/sumin/stream/PacketTypeInfo.java
+++ src/main/java/org/sumin/stream/PacketTypeInfo.java
@@ -11,6 +11,6 @@ import java.util.Map;
 public class PacketTypeInfo extends TypeInfoFactory<List> {
     @Override
     public TypeInformation<List> createTypeInfo(Type type, Map<String, TypeInformation<?>> map) {
-        return new ListTypeInfo(TypeInformation.of(String.class));
+        return new ListTypeInfo(TypeInformation.of(Integer.class));
     }
 }
